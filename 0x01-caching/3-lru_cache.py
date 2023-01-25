b79cd1afe81965e612bff8d81df152a29c80b9e3
abrhamgg <abrhamgg@gmail.com>
2023-01-25 22:14:48 +0300

    completed task 3 lru caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+"""LRU caching"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """inherits BaseCaching"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """inserts item in the cache and if the cache is
+        full remove the first element"""
+        if key and item:
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                    print("before discarding")
+                    print(self.cache_data)
+                    lru_key, _ = self.cache_data.popitem(True)
+                    print("DISCARD: {}".format(lru_key))
+                print(self.cache_data)
+                self.cache_data[key] = item
+                self.cache_data.move_to_end(key, last=False)
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves item by key"""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
