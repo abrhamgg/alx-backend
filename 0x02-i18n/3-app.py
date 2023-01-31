29abbe52a7fcbbe40aa7b95cf8058c64b0313194
abrhamgg <abrhamgg@gmail.com>
2023-01-31 23:41:41 +0300

    task 3 completed

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""get locale"""
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+
+
+class Config(object):
+    """config class for flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def index():
+    """route for default page"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
